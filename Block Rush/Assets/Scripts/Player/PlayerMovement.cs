09d50fc111ed5bf09da28c1a16beaf96f9589eb9
Remco <rempjuh@hotmail.com>
2017-05-20 21:58:40 +0200

    Added player movent

---
+++ Block Rush/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	[SerializeField]
+	private SimpleTouchController _leftJoyStick;
+	[SerializeField]
+	private SimpleTouchController _rightJoyStick;
+	private Rigidbody _rigidBody;
+	private const float _movementSpeed = 5;
+	// Use this for initialization
+	void Start () {
+		_rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_rigidBody.MovePosition(transform.position + (Vector3.forward * _leftJoyStick.GetTouchPosition.y * Time.deltaTime * _movementSpeed) +
+		(Vector3.right * _leftJoyStick.GetTouchPosition.x * Time.deltaTime * _movementSpeed));
+
+		transform.LookAt(transform.position + new Vector3(_rightJoyStick.GetTouchPosition.x, 0, _rightJoyStick.GetTouchPosition.y));
+	}
+}
